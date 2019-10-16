fea4534789089932becaad91eeeefdefd108588f
Adam Rumiński <Adam.Ruminski@atena.pl>
2019-10-16 10:14:49 +0200

    100%

--- src/Solution.java
+++ src/Solution.java
@@ -5,35 +5,39 @@ import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
-import java.util.stream.IntStream;
 
 public class Solution {
 
-    // Complete the staircase function below.
-    static void staircase(int n) {
-        StringBuffer sb = new StringBuffer();
-        char s = ' ';
-        char h = '#';
-        for(int i = 0; i<n; i++) {
-            for (int j = n-1; j>=0; j--) {
-                if (j <= i) {
-                    sb.append(h);
-                } else {
-                    sb.append(s);
-                }
-            }
-            sb.append('\n');
-        }
-        System.out.println(sb.toString());
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        BigInteger sum = Arrays.stream(arr)
+                .mapToObj(BigInteger::valueOf)
+                .reduce(BigInteger.valueOf(0), BigInteger::add);
+        BigInteger min = sum.subtract(
+                BigInteger.valueOf(Arrays.stream(arr)
+                        .max()
+                        .orElse(0)));
+        BigInteger max = sum.subtract(
+                BigInteger.valueOf(Arrays.stream(arr)
+                        .min()
+                        .orElse(0)));
+        System.out.println(min.toString() + " " + max.toString());
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int n = scanner.nextInt();
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        staircase(n);
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
 
         scanner.close();
     }
