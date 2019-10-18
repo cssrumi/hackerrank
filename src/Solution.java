4a260b5c23eefff05f2ba0027da19d68949526c5
Adam Rumiński <Adam.Ruminski@atena.pl>
2019-10-18 13:25:01 +0200

    100%

--- src/Solution.java
+++ src/Solution.java
@@ -1,186 +1,73 @@
-import java.io.IOException;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
 
-    private static class Point {
-        int x, y;
-
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
+    // Complete the minimumLoss function below.
+    static int minimumLoss2(long[] price) {
+        Integer min = null;
+        Long tempMin;
+        for (int i = 0; i < price.length - 1; i++) {
+            long currentPrice = price[i];
+            tempMin = Arrays.stream(price)
+                    .skip(i + 1)
+                    .map(next -> currentPrice - next)
+                    .filter(e -> e >= 0)
+                    .min()
+                    .orElse(-1);
+            if (min == null)
+                min = tempMin.intValue();
+            if (tempMin != -1 && tempMin < min)
+                min = tempMin.intValue();
         }
-
-        static Point from(int x, int y) {
-            return new Point(x, y);
-        }
-
-        boolean checkBounds(int from, int to) {
-            if (x < from || x > to) return false;
-            if (y < from || y > to) return false;
-            return true;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Point point = (Point) o;
-
-            if (x != point.x) return false;
-            return y == point.y;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = x;
-            result = 31 * result + y;
-            return result;
-        }
+        return min;
     }
 
-    private static class Move {
-        int dx, dy;
-
-        Move(int dx, int dy) {
-            this.dx = dx;
-            this.dy = dy;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Move move = (Move) o;
-
-            if (dx != move.dx) return false;
-            return dy == move.dy;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = dx;
-            result = 31 * result + dy;
-            return result;
-        }
-    }
-
-    private static class Pattern {
-        int dx, dy, shortestDist;
-        Set<Move> moves;
-
-        Pattern(int dx, int dy) {
-            this.dx = dx;
-            this.dy = dy;
-            initMoves();
-        }
-
-        private void initMoves() {
-            moves = new HashSet<>();
-            moves.add(new Move(dx, dy));
-            moves.add(new Move(dx, -dy));
-            moves.add(new Move(-dx, dy));
-            moves.add(new Move(-dx, -dy));
-            moves.add(new Move(dy, dx));
-            moves.add(new Move(dy, -dx));
-            moves.add(new Move(-dy, dx));
-            moves.add(new Move(-dy, -dx));
-        }
-
-        private static List<Pattern> getAllPatterns(int from, int to) {
-            List<Pattern> patterns = new ArrayList<>();
-            for (int i = from; i <= to; i++) {
-                for (int j = from; j <= to; j++) {
-                    patterns.add(new Pattern(i, j));
-                }
-            }
-            return patterns;
-        }
-
-        private void calculateShortestDist(Point start, Point stop, int size) {
-            HashMap<Point, Integer> dist = new HashMap<>();
-            Deque<Point> deque = new LinkedList<>();
-
-            Point next;
-
-            deque.add(start);
-            dist.put(start, 0);
-
-            while (!deque.isEmpty()) {
-                Point current = deque.pop();
-                if (current.equals(stop)) {
-                    shortestDist = dist.get(current);
-                    break;
-                }
-
-                for (Move move : moves) {
-                    next = Point.from(current.x + move.dx, current.y + move.dy);
-                    if (next.checkBounds(0, size - 1) && !dist.containsKey(next)) {
-                        dist.put(next, dist.get(current) + 1);
-                        deque.add(next);
-                    }
-                }
-                if (shortestDist == 0) shortestDist = -1;
+    static int minimumLoss3(long[] price) {
+        long min = Long.MAX_VALUE;
+        for (int current = 0; current < price.length - 1; current++) {
+            long difference;
+            for (int next = current + 1; next < price.length; next++) {
+                difference = price[current] - price[next];
+                if (difference > 0 && difference < min)
+                    min = difference;
             }
         }
+        return (int) min;
     }
 
-
-    // Complete the knightlOnAChessboard function below.
-    static int[][] knightlOnAChessboard(int n) {
-        int[][] result = new int[n - 1][n - 1];
-        Point start = Point.from(0, 0);
-        Point stop = Point.from(n - 1, n - 1);
-
-        List<Pattern> patterns = Pattern.getAllPatterns(1, n - 1);
-
-        for (Pattern pattern : patterns) {
-            pattern.calculateShortestDist(start, stop, n);
-            result[pattern.dx - 1][pattern.dy - 1] = pattern.shortestDist;
+    static int minimumLoss(long[] price) {
+        HashMap<Long, Integer> position = new HashMap<>();
+        long min = Long.MAX_VALUE;
+        long diff, cur, next;
+        for (int i = 0; i < price.length; i++) {
+            position.put(price[i], i);
         }
 
-        return result;
+        Arrays.sort(price);
+
+        for (int i = price.length - 1; i > 0; i--) {
+            cur = price[i];
+            next = price[i - 1];
+            if (position.get(cur) < position.get(next)) {
+                diff = cur - next;
+                if (diff < min) min = diff;
+            }
+        }
+
+        return (int) min;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        int n = 5;
-        int[][] result = knightlOnAChessboard(n);
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1; j++) {
-                System.out.print(result[i][j]);
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
-//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-//
-//        int n = scanner.nextInt();
-//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-//
-//        int[][] result = knightlOnAChessboard(n);
-//
-//        for (int i = 0; i < result.length; i++) {
-//            for (int j = 0; j < result[i].length; j++) {
-//                bufferedWriter.write(String.valueOf(result[i][j]));
-//
-//                if (j != result[i].length - 1) {
-//                    bufferedWriter.write(" ");
-//                }
-//            }
-//
-//            if (i != result.length - 1) {
-//                bufferedWriter.write("\n");
-//            }
-//        }
-//
-//        bufferedWriter.newLine();
-//
-//        bufferedWriter.close();
-//
-//        scanner.close();
+        long[] price = {20, 7, 8, 2, 5};
+        int result = minimumLoss(price);
+        System.out.println(result);
     }
 }
