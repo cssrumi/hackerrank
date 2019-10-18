453f6d198ccac73d2c392cd60c30f446c2211e1a
Adam Rumiński <Adam.Ruminski@atena.pl>
2019-10-18 15:59:54 +0200

    100%

--- src/Solution.java
+++ src/Solution.java
@@ -9,21 +9,68 @@ import java.util.stream.Collectors;
 
 public class Solution {
 
-    // Complete the missingNumbers function below.
-    static int[] missingNumbers(int[] arr, int[] brr) {
-        List<Integer> all = Arrays.stream(brr).boxed().collect(Collectors.toList());
+    // Complete the pairs function below.
+    static int pairs2(int k, int[] arr) {
+        int results = 0;
+        for (int current : arr) {
+            for (int next : arr) {
+                if (current - next == k) {
+                    results++;
+                }
+            }
+        }
+        return results;
+    }
+    // Complete the pairs function below.
+    static int pairs3(int k, int[] arr) {
+        List<Integer> sorted;
+        int results = 0;
+        if (k < 0)
+            sorted = Arrays.stream(arr).boxed().sorted().collect(Collectors.toList());
+        else
+            sorted = Arrays.stream(arr).boxed().sorted(Collections.reverseOrder()).collect(Collectors.toList());
+        for (int current = 0; current < sorted.size() - 1; current++) {
+            if (sorted.get(current) - sorted.get(current + 1) > k) continue;
+            for (int next = current + 1; next < sorted.size(); next++) {
+                if (sorted.get(current) - sorted.get(next) == k) {
+                    results++;
+                    break;
+                }
+            }
+        }
+        return results;
+    }
 
-        Arrays.stream(arr).boxed().forEach(all::remove);
+    // Complete the pairs function below.
+    static int pairs(int k, int[] arr) {
+        List<Integer> sorted;
+        int results = 0;
+        int next;
+        if (k < 0)
+            sorted = Arrays.stream(arr).boxed().sorted().collect(Collectors.toList());
+        else
+            sorted = Arrays.stream(arr).boxed().sorted(Collections.reverseOrder()).collect(Collectors.toList());
+        for (int current = 0; current < sorted.size() - 1; current++) {
 
-        return new HashSet<>(all).stream().sorted().mapToInt(e -> e).toArray();
+            next = current + 1;
+            while (sorted.get(current) - sorted.get(next) <= k) {
+                if (sorted.get(current) - sorted.get(next) == k) {
+                    results++;
+                    break;
+                }
+                next++;
+                if (next >= sorted.size()) break;
+            }
+        }
+        return results;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        int[] arr = {203, 204, 205, 206, 207, 208, 203, 204, 205, 206};
-        int[] brr = {203, 204, 205, 206, 207, 208, 203, 204, 205, 206};
-        int[] result = missingNumbers(arr, brr);
-        Arrays.stream(result).forEach(System.out::println);
+        int[] arr = {1, 5, 3, 4, 2};
+
+        int result = pairs(2, arr);
+        System.out.println(result);
     }
 }
