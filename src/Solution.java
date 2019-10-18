0f2d6126cc371937118539a777777e52f121270c
Adam Ruminski <cssrumi@gmail.com>
2019-10-18 21:26:50 +0200

    100%

--- src/Solution.java
+++ src/Solution.java
@@ -9,68 +9,33 @@ import java.util.stream.Collectors;
 
 public class Solution {
 
-    // Complete the pairs function below.
-    static int pairs2(int k, int[] arr) {
-        int results = 0;
-        for (int current : arr) {
-            for (int next : arr) {
-                if (current - next == k) {
-                    results++;
-                }
+    // Complete the balancedSums function below.
+    static String balancedSums(List<Integer> arr) {
+        if (arr.size() == 1) return "YES";
+        int sum = arr.stream().reduce(0, Integer::sum);
+        int count = 0;
+        int current;
+        for (int i = 0; i < arr.size(); i++) {
+            current = arr.get(i);
+            if (count == sum - count - current) {
+                return "YES";
             }
+            count += current;
         }
-        return results;
-    }
-    // Complete the pairs function below.
-    static int pairs3(int k, int[] arr) {
-        List<Integer> sorted;
-        int results = 0;
-        if (k < 0)
-            sorted = Arrays.stream(arr).boxed().sorted().collect(Collectors.toList());
-        else
-            sorted = Arrays.stream(arr).boxed().sorted(Collections.reverseOrder()).collect(Collectors.toList());
-        for (int current = 0; current < sorted.size() - 1; current++) {
-            if (sorted.get(current) - sorted.get(current + 1) > k) continue;
-            for (int next = current + 1; next < sorted.size(); next++) {
-                if (sorted.get(current) - sorted.get(next) == k) {
-                    results++;
-                    break;
-                }
-            }
-        }
-        return results;
+        return "NO";
     }
 
-    // Complete the pairs function below.
-    static int pairs(int k, int[] arr) {
-        List<Integer> sorted;
-        int results = 0;
-        int next;
-        if (k < 0)
-            sorted = Arrays.stream(arr).boxed().sorted().collect(Collectors.toList());
-        else
-            sorted = Arrays.stream(arr).boxed().sorted(Collections.reverseOrder()).collect(Collectors.toList());
-        for (int current = 0; current < sorted.size() - 1; current++) {
-
-            next = current + 1;
-            while (sorted.get(current) - sorted.get(next) <= k) {
-                if (sorted.get(current) - sorted.get(next) == k) {
-                    results++;
-                    break;
-                }
-                next++;
-                if (next >= sorted.size()) break;
-            }
-        }
-        return results;
-    }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        int[] arr = {1, 5, 3, 4, 2};
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(3);
 
-        int result = pairs(2, arr);
+        String result = balancedSums(arr);
         System.out.println(result);
     }
 }
