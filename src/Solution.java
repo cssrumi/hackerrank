ccc3b5660d96289295aca53db602fbe0d98977ce
Adam Rumiński <Adam.Ruminski@atena.pl>
2019-10-24 16:58:26 +0200

    0%

--- src/Solution.java
+++ src/Solution.java
@@ -9,8 +9,39 @@ import java.util.stream.Collectors;
 
 public class Solution {
 
-    static int simpleArraySum(int[] ar) {
-        return Arrays.stream(ar).reduce(0, Integer::sum);
+
+    static List<List<Integer>> createLayers(List<List<Integer>> matrix) {
+        List<List<Integer>> layers = new ArrayList<>();
+
+        int columns = matrix.size();
+        int rows = matrix.get(0).size();
+
+        int lowerValue = Integer.min(columns, rows);
+        int layersSize = lowerValue % 2 == 1 ? (lowerValue + 1) / 2 : lowerValue / 2;
+
+        for (int i = 1; i <= layersSize; i++) {
+            List<Integer> layer = new ArrayList<>();
+            for (int column = columns - i; column >= i - 1; column--) {
+                layer.add(matrix.get(i - 1).get(column));
+            }
+            for (int row = i - 1; i >= rows - i; row++) {
+                layer.add(matrix.get(row).get(i - 1));
+            }
+        }
+
+    }
+
+    static List<List<Integer>> fromLayerToList(List<List<Integer>> layers) {
+
+    }
+
+    // Complete the matrixRotation function below.
+    static void matrixRotation(List<List<Integer>> matrix, int r) {
+        List<List<Integer>> layers = createLayers(matrix);
+        int realR;
+        for (List<Integer> layer: layers) {
+            realR = r % layer.size();
+        }
     }
 
     private static final Scanner scanner = new Scanner(System.in);
