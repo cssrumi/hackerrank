9a88ca4ccab51d3a7787a433a60a8e88dd83caf4
Adam Ruminski <cssrumi@gmail.com>
2019-10-26 16:18:50 +0200

    100%

--- src/Solution.java
+++ src/Solution.java
@@ -5,41 +5,13 @@ import java.util.*;
 
 public class Solution {
 
-    static boolean isAllNextUnique(String str) {
-        if (str.length() == 0) return false;
-        CharacterIterator iter = new StringCharacterIterator(str);
-        char cur = iter.current();
-
-        while (iter.next() != CharacterIterator.DONE) {
-            if (cur == iter.current()) return false;
-            cur = iter.current();
+    static int camelcase(String s) {
+        CharacterIterator i = new StringCharacterIterator(s);
+        int counter = 1;
+        while (i.current() != CharacterIterator.DONE) {
+            if (Character.isUpperCase(i.next())) counter++;
         }
-        return true;
-    }
-
-    static String superReducedString(String s) {
-        String emptyString = "Empty String";
-        String tempString = s;
-
-        CharacterIterator iter;
-        char cur;
-        int counter;
-
-        while (!isAllNextUnique(tempString)) {
-            if (tempString.length() == 0) return emptyString;
-            iter = new StringCharacterIterator(tempString);
-            cur = iter.current();
-            counter = 0;
-            while (iter.next() != CharacterIterator.DONE) {
-                if (cur == iter.current()) {
-                    tempString = tempString.substring(0, counter) + tempString.substring(counter + 2);
-                    break;
-                }
-                cur = iter.current();
-                counter++;
-            }
-        }
-        return tempString;
+        return counter;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -50,8 +22,8 @@ public class Solution {
 //        List<Integer> list = Arrays.asList(73, 67, 38, 33);
 //        List<Integer> result = gradingStudents(list);
 
-        String str = "baaddcrrcb";
-        String result = superReducedString(str);
+        String str = "baaddCrrcEb";
+        int result = camelcase(str);
         System.out.println(result);
     }
 }
