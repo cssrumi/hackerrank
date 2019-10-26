8ec5de52f87a7f80418f79dfe599bef7972e749a
Adam Ruminski <cssrumi@gmail.com>
2019-10-26 19:01:18 +0200

    fail

--- src/Solution.java
+++ src/Solution.java
@@ -2,43 +2,78 @@ import java.io.*;
 import java.text.CharacterIterator;
 import java.text.StringCharacterIterator;
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Map.Entry.comparingByValue;
+import static java.util.stream.Collectors.toMap;
 
 public class Solution {
 
-    static int minimumNumber(int n, String password) {
-        String specialCharacters = "!@#$%^&*()-+";
-        int minimumNum = Math.max(6 - n, 0);
-        boolean isUpper = false;
-        boolean isLower = false;
-        boolean isNumber = false;
-        boolean isSpecialCharacter = false;
-        int checkSum = 4;
+    static Optional<Character> findDoubled(List<Character> characters) {
+        if (characters.size() == 0) return Optional.empty();
+        char ch = characters.get(0);
 
-        CharacterIterator i = new StringCharacterIterator(password);
+        for (int i = 1; i < characters.size(); i++) {
+            if (ch == characters.get(i)) return Optional.of(ch);
+            ch = characters.get(i);
+        }
+        return Optional.empty();
+    }
 
-        while (i.current() != CharacterIterator.DONE) {
-            if (Character.isUpperCase(i.current()) && !isUpper) {
-                isUpper = true;
-                checkSum--;
+    static HashMap<Character, Integer> genCharactersCountMap(List<Character> characters) {
+        Set<Character> characterSet = new HashSet<>(characters);
+        HashMap<Character, Integer> characterCount = new HashMap<>();
+        for (Character character : characters) {
+            if (characterCount.containsKey(character)) {
+                characterCount.put(character, characterCount.get(character) + 1);
+            } else {
+                characterCount.put(character, 1);
             }
-            if (Character.isLowerCase(i.current()) && !isLower) {
-                isLower = true;
-                checkSum--;
+        }
+        return characterCount;
+    }
+
+    static int alternate(String s) {
+        List<Character> characters = s.chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+        Set<Character> uniqueCharacters;
+        List<Queue<Character>> allPermutations;
+        int uniqueCharactersSize;
+        int maxSize;
+
+        Optional<Character> toRemove = findDoubled(characters);
+        while (toRemove.isPresent()) {
+            while (characters.contains(toRemove.get())) {
+                characters.remove(toRemove.get());
             }
-            if (Character.isDigit(i.current()) && !isNumber) {
-                isNumber = true;
-                checkSum--;
-            }
-            if (specialCharacters.indexOf(i.current()) != -1 && !isSpecialCharacter) {
-                isSpecialCharacter = true;
-                checkSum--;
-            }
-            i.next();
+            toRemove = findDoubled(characters);
         }
 
-        minimumNum = Math.max(minimumNum, checkSum);
+        maxSize = characters.size();
 
-        return minimumNum;
+        uniqueCharacters = new HashSet<>(characters);
+        uniqueCharactersSize = uniqueCharacters.size();
+        if (uniqueCharactersSize == 2) return maxSize;
+        if (uniqueCharactersSize < 2) return 0;
+
+        allPermutations = genAllPermutations(uniqueCharacters);
+
+        return maxSize;
+
+    }
+
+    private static long factorialUsingRecursion(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        return n * factorialUsingRecursion(n - 1);
+    }
+
+    private static List<Queue<Character>> genAllPermutations(Set<Character> uniqueCharacters) {
+        Set<Queue<Character>> allPermutations = new HashSet<>();
+        long counter = factorialUsingRecursion(uniqueCharacters.size());
+        while (counter != 0) {
+
+        }
     }
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -49,8 +84,8 @@ public class Solution {
 //        List<Integer> list = Arrays.asList(73, 67, 38, 33);
 //        List<Integer> result = gradingStudents(list);
 
-        String str = "#HackerRank";
-        int result = minimumNumber(11, str);
+        String str = "beabeefeab";
+        int result = alternate(str);
         System.out.println(result);
     }
 }
